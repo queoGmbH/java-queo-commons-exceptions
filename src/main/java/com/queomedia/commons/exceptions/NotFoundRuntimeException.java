7240ba2d9163021fdb3ca452060edff89da65e21
Ralph Engelmann <r.engelmann@queo-group.com>
2009-11-16 15:01:28 +0000

    Initial Import

---
+++ src/main/java/com/queomedia/commons/exceptions/NotFoundRuntimeException.java
@@ -0,0 +1,47 @@
+package com.queomedia.commons.exceptions;
+
+/**
+ * Thrown to indicate that an object is not found.
+ * 
+ * @author Engelmann
+ */
+public class NotFoundRuntimeException extends RuntimeException {
+
+    /** The Constant serialVersionUID. */
+    private static final long serialVersionUID = -4125626288727236784L;
+
+    /**
+     * Instantiates a new not found exception.
+     */
+    public NotFoundRuntimeException() {
+    }
+
+    /**
+     * Instantiates a new not found exception.
+     * 
+     * @param message the message
+     */
+    public NotFoundRuntimeException(final String message) {
+        super(message);
+    }
+
+    /**
+     * Instantiates a new not found exception.
+     * 
+     * @param cause the cause
+     */
+    public NotFoundRuntimeException(final Throwable cause) {
+        super(cause);
+    }
+
+    /**
+     * Instantiates a new not found exception.
+     * 
+     * @param message the message
+     * @param cause the cause
+     */
+    public NotFoundRuntimeException(final String message, final Throwable cause) {
+        super(message, cause);
+    }
+
+}
